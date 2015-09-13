2743e204cc1a0b7fb884fb4e5d5ce4b370efc8ed
maXchine <maxchine@aliyun.com>
2015-09-13 22:48:45 +0800

    Add something.

---
+++ Nuty.Authorization/Context/AuthContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Nuty.Authorization.Context {
+
+    using System.Data.Entity;
+    public class AuthContext : DbContext {
+
+        public AuthContext(string connectionString) : base(connectionString) { }
+
+        public DataSet<>
+    }
+}
---
+++ Nuty.Authorization/Module/Controller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nuty.Authorization.Module {
+    public class Controller {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public virtual ICollection<Action> Actions { get; set; }
+    }
+}
