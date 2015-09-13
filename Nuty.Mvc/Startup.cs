1720c46eb2dab530ca4d59f5855d5e48d031de35
maXchine <maxchine@aliyun.com>
2015-09-14 00:26:21 +0800

    Web

---
+++ Nuty.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nuty.Mvc.Startup))]
+namespace Nuty.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
